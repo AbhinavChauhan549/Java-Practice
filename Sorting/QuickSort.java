f477b2facebd69b81d0f50abf6920bc9e25193a3
Abhinav Chauhan <officialabhi549@gmail.com>
2026-04-17 12:11:02 +0530

    quick sort

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,6 +1,43 @@
 import java.util.Scanner;
 
 public class QuickSort {
+
+  static void quickSort(int[] arr, int low, int high) {
+
+    if (low >= high) {
+      return;
+    }
+    int s = low;
+    int e = high;
+
+    int mid = s + (e - s) / 2;
+
+    int pivot = arr[mid];
+
+    while (s <= e) {
+      while (arr[s] < arr[mid]) {
+        s++;
+      }
+      while (arr[e] > arr[mid]) {
+        e--;
+      }
+      // if condition to make sure that s<=e is not disturbed
+      if (s <= e) {
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+        s++;
+        e--;
+      }
+    }
+
+    // now my pivot is at correct index , now repeat this step for array that is at
+    // left and right side of pivot
+    quickSort(arr, s, high);
+    quickSort(arr, low, e);
+
+  }
+
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
     int n;
@@ -21,6 +58,12 @@ public class QuickSort {
     }
 
     System.out.println("Quick Sort : ");
+    quickSort(arr, 0, arr.length - 1);
+
+    for (int a : arr) {
+      System.out.println(a);
+
+    }
 
   }
 }
