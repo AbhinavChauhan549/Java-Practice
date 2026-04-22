af59e0454555390d803ea05c75f2a234fa16c891
Abhinav Chauhan <officialabhi549@gmail.com>
2026-04-22 16:59:30 +0530

    lambda 2

--- Java8/LambdaFunction/SortStringByLength.java
+++ Java8/LambdaFunction/SortStringByLength.java
@@ -1,5 +1,17 @@
 package Java8.LambdaFunction;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SortStringByLength {
 
+  public static void main(String[] args) {
+
+    // Sort a list of strings based on their length using a lambda expression.
+    List<String> names = Arrays.asList("Java", "Python", "C", "JavaScript");
+
+    names.sort((a, b) -> a.length() - b.length());
+    System.out.println(names);
+
+  }
 }
